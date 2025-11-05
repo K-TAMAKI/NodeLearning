81c6c417c0f72c4084d89ef8fa46b63e925d6001
K-TAMAKI <sirius8.6knvenus@gmail.com>
2025-11-05 18:39:02 +0900

    第15章　メタデータの追加まで

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
+//import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next';
 
+//import { neon } from '@neondatabase/serverless';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
+//export const experimental_ppr = true;
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape} />
+      {/* <div className={styles.shape} /> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /*experimental: {
+    ppr: 'incremental'
+  },*/
+  experimental: {
+    cacheComponents: true
+  }
+  /*experimental: {
+    cacheComponents: 'incremental'
+  },*/
 };
+
 
 export default nextConfig;
